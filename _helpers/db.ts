8744ac3a5caf6dabb5ba7cf82a6e561a80117bbe
ancline <anclineseaborg68@gmail.com>
2026-05-17 13:34:25 +0800

    Fix Sequelize host and port config

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -10,25 +10,27 @@ export default db;
 initialize();
 
 async function initialize() {
-const { host, port, user, password, database } = config.database; 
-const connection = await mysql.createConnection({host, port, user, password });
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
 
-// Create DB if it doesn't exist
-
+    // Create DB if it doesn't exist
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // Connect to DB
-    const sequelize = new Sequelize(database, user, password, {dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, {
+        host,
+        port,
+        dialect: 'mysql'
+    });
 
     // Init models
     db.Account = accountModel(sequelize);
     db.RefreshToken = refreshTokenModel(sequelize);
 
     // Define relationships
-    db.Account.hasMany (db.RefreshToken, { onDelete: 'CASCADE' });
+    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
 
     // Sync models with database
     await sequelize.sync();
-
 }
